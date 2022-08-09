a24987d7d1f028b58be08eace32bbbb10a693e88
Pulkitkhanna <pulkitkhanna1@gmail.com>
2022-08-09 21:42:24 +0530

    chart

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './NewsFeed.css'
+import LineGraph from './LineGraph.js';
 
 function NewsFeed() {
   return (
     <div className='NewFeed'>
         <div className="NewFeed_Container">
-            news
+            <div className="newsfeed_chartSection">
+                <div className="newsfeed_portfolio">
+                    <h1 className='folio_value'>$120,000</h1>
+                    <p>+$44.53 (+.04%) Today</p>
+                </div>
+                <div className="newsfeed_chart">
+                    <LineGraph />
+                </div>
+            </div>
         </div>
     </div>
   )
